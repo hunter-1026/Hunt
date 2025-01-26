342ea1630d86a9fccb6a305539237ca5bd9876c3
hunter-1026 <dirosebrijesh@gmail.com>
2025-01-26 13:28:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Atomic:Atomic3092006@Ro@a
 DB_NAME = os.environ.get("DATABASE_NAME", "hunt")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002324048110"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002299699672"))
 )
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002073897814"))
 
